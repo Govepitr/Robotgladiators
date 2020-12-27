9b51c895d40cc8d2e760b66a4d2afa7dd1c63d12
Dan Arbelo <govepitr@gmail.com>
2020-12-26 19:12:48 -0700

    pray for mojo.. I mean they now fight 1v1

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,13 +2,14 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-//Let's not forget to drop a log!
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Boruto";
-var enemyHealth =50;
+var enemyName = "Roborto";
+var enemyHealth = 50;
 var enemyAttack = 12;
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+console.log(enemyName, enemyHealth, enemyAttack);
 
+//create function
 var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
@@ -20,8 +21,7 @@ var fight = function() {
   console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
-  
-// check enemy's health
+  // check enemy's health
 if (enemyHealth <= 0) {
   window.alert(enemyName + " has died!");
 } 
@@ -36,18 +36,13 @@ else {
   console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
-  // put new code under this
-console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-
-// check player's health
+  // check player's health
 if (playerHealth <= 0) {
   window.alert(playerName + " has died!");
 } 
 else {
   window.alert(playerName + " still has " + playerHealth + " health left.");
 }
-
-
 };
-
+//execute function
 fight();
