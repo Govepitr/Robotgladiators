99dc01e72c365536c8faace144f9705721708a60
Dan Arbelo <govepitr@gmail.com>
2020-12-26 18:15:29 -0700

    Log droppings

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert, do you see it? Huh? Go ya!??!?!?");
+var playerName = window.prompt("What is your robot's name?");
+  console.log(playerName);
+  console.log("Just dropping logs left and right to leave messages to myself and others.");
+  console.log(10 + 10);
+  console.log("Our robot's name is " + playerName);
+
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+
+// fight();
