e2f93327d62d549759a1583fe5936e57bc7b5404
Dan Arbelo <govepitr@gmail.com>
2020-12-26 22:56:42 -0700

    Groundhound's Day - Add enemy Combat

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+  
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //    * Fight all enemy-robots
@@ -11,23 +12,12 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-// // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth, playerMoney);
-console.log(enemyNames[i], enemyHealth, enemyAttack);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
-}
 
 //create function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-      window.alert("Welcome to Robot Gladiators!");
+                  // ^---(enemyName is the fight functions' parameter
+  //fight function statements
+
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -57,27 +47,34 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
   // if player choses to skip
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-  window.alert(playerName + " has chosen to skip the fight!");
-  // confirm player wants to skip
-  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  } else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-  // if yes (true), leave fight
-  if (confirmSkip) {
-    window.alert(playerName + " has decided to skip this fight. Goodbye!");
-    // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
-  }
-  // if no (false), ask question again by running fight() again
-  else {
-    fight();
-  }
-} else {
-  window.alert("You need to choose a valid option. Try again!");
-}
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
+    } else {
+      window.alert("You need to choose a valid option. Try again!");
+      }
 };
 for(var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]);
 }
+// // You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth, playerMoney);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames[i], enemyHealth, enemyAttack);
+console.log(enemyNames.length);
 //execute function
 // fight();
